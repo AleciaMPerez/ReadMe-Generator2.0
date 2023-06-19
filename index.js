450b8af3e070f0428dd5df31c96dfbaab569c57e
AleciaMPerez <alecia.perez@hotmail.com>
2023-06-19 11:01:07 -0400

    wrote out questions

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?",
+      },
+      {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'List a table of contents',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Advise the steps required to install your project',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Proide instructions and examples of use',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Cite credits',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'list license',
+      },
+
+]);
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
