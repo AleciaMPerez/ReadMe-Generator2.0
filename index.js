4a81a1b56829a25af060768c8a7d3c7b834a3de4
AleciaMPerez <alecia.perez@hotmail.com>
2023-06-21 14:32:50 -0400

    function to initalize app

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-
+return inquirer.createPromptModule(questions)
 }
 
 // Function call to initialize app
